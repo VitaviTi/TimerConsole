cd343cb60d3728c0d4421767f6551c16cad737aa
VitaviTi <vitavi294ti@gmail.com>
2024-05-28 21:10:36 +0300

    Timer final version

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <windows.h>
+#include <cstring>
+
+int main()
+{
+    std::string n;
+    std::cout<< "Enter second: "; std::cin>> n;
+    for(int i=0; i< stoi(n); i++)
+    {
+        system("cls");
+        std::cout<< stoi(n) - i;
+        Sleep(1000);
+    }
+    system("cls");
+    std::cout << "Ok" << std::endl;
+    return 0;
+}
